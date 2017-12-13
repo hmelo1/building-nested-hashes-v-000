ff31abe46a3d4cf47610ec043480e4f4d7de858f
Eddie Melo <heriberto@melo.nyc>
2017-12-13 12:59:24 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,19 +7,21 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {name: "Benvolio", age: "17", status: "worried"},
-        {name: "Mercutio", age: "18", status: "hot-headed"}          
+        {name: "Mercutio", age: "18", status: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {name: "Steven", age: "30", status: "confused"},
+        {name: "Nurse", age:"44", status: "worried"}
       ]
    }
 }
 
-  
+
 
 end
