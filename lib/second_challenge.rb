7626fe1d66b0c16671626974e37d36202fb6f3b3
Eddie Melo <heriberto@melo.nyc>
2017-12-13 13:03:36 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -18,8 +18,12 @@ def second_challenge
       hero_friends =>{
         {
           :name => "Benvolio",
-          :age => "17"
+          :age => "17",
           :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
         }
       }
       },
