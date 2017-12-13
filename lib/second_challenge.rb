28870f9dff2539df660d7d4c7e2c02637fa953a6
Eddie Melo <heriberto@melo.nyc>
2017-12-13 13:01:53 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,12 @@ def second_challenge
       patriarch => {
         :name => "Lord Montague",
         :age => "53"
-      }
+      },
+      matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      hero
       },
     :capulet => {
       #your key/value pairs here!
